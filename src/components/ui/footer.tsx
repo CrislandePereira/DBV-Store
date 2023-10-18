f53dcc0bc68c3225509758955b8cf9f9f85708e4
Crislande Pereira <cris_mg1994@hotmail.com>
2023-10-18 14:26:49 -0300

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+      © 2023 - All rights reserved -{" "}
+      <span className="font-semibold"> Cris Pereira</span>
+    </div>
+  );
+};
+
+export default Footer;

9d6972e6e596fa96842a69087274ef6b4016e0fe
Crislande Pereira <cris_mg1994@hotmail.com>
2023-10-18 13:51:59 -0300

    feat: add ProductItem

--- src/components/ui/productItem.tsx
+++ src/components/ui/productItem.tsx
@@ -1,14 +1,21 @@
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
+import { Badge } from "./badge";
+import { ArrowDownIcon } from "lucide-react";
 
 interface ProductItemProps {
-  product: Product;
+  product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+            <ArrowDownIcon size={14} /> {product.discountPercentage}%
+          </Badge>
+        )}
         <Image
           src={product.imageUrls[0]}
           height={0}
@@ -22,10 +29,27 @@ const ProductItem = ({ product }: ProductItemProps) => {
         />
       </div>
 
-      <div>
+      <div className="flex flex-col gap-1">
         <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
           {product.name}
         </p>
+
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="font-bold">
+                R$ {Number(product.totalPrice).toFixed(2)}
+              </p>
+              <p className="text-xs text-gray-400 line-through">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="text-sm font-bold">
+              R$ {Number(product.basePrice).toFixed(2)}
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );

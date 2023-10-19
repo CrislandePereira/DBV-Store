720eb7e4ceef20ad055539db0399014ca076e500
Crislande Pereira <cris_mg1994@hotmail.com>
2023-10-19 10:29:06 -0300

    feat: ensure user goes to product detail when click to product

---
+++ src/app/product/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { prismaClient } from "@/lib/prisma";
+
+interface ProductDetailsPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const ProductDetailsPage = async ({
+  params: { slug },
+}: ProductDetailsPageProps) => {
+  const product = await prismaClient.product.findFirst({
+    where: {
+      slug: slug,
+    },
+  });
+
+  if (!product) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <h1>{product?.name}</h1>
+    </div>
+  );
+};
+
+export default ProductDetailsPage;
--- src/components/ui/productItem.tsx
+++ src/components/ui/productItem.tsx
@@ -2,6 +2,7 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
@@ -9,49 +10,51 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
-            <ArrowDownIcon size={14} /> {product.discountPercentage}%
-          </Badge>
-        )}
-        <Image
-          src={product.imageUrls[0]}
-          height={0}
-          width={0}
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          sizes="100vw"
-          style={{
-            objectFit: "contain",
-          }}
-          alt={product.name}
-        />
-      </div>
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex flex-col gap-4">
+        <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+              <ArrowDownIcon size={14} /> {product.discountPercentage}%
+            </Badge>
+          )}
+          <Image
+            src={product.imageUrls[0]}
+            height={0}
+            width={0}
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            sizes="100vw"
+            style={{
+              objectFit: "contain",
+            }}
+            alt={product.name}
+          />
+        </div>
 
-      <div className="flex flex-col gap-1">
-        <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-          {product.name}
-        </p>
+        <div className="flex flex-col gap-1">
+          <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+            {product.name}
+          </p>
 
-        <div className="flex items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap">
-          {product.discountPercentage > 0 ? (
-            <>
+          <div className="flex items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="font-bold">
+                  R$ {Number(product.totalPrice).toFixed(2).replace(".", ",")}
+                </p>
+                <p className="text-xs text-gray-400 line-through">
+                  R$ {Number(product.basePrice).toFixed(2).replace(".", ",")}
+                </p>
+              </>
+            ) : (
               <p className="font-bold">
-                R$ {Number(product.totalPrice).toFixed(2).replace(".", ",")}
-              </p>
-              <p className="text-xs text-gray-400 line-through">
                 R$ {Number(product.basePrice).toFixed(2).replace(".", ",")}
               </p>
-            </>
-          ) : (
-            <p className="font-bold">
-              R$ {Number(product.basePrice).toFixed(2).replace(".", ",")}
-            </p>
-          )}
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
